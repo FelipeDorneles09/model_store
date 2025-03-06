fd2a6f0946ca7380127d12abff13b2ce0efefcfa
Felipe <fdorneles63@gmail.com>
2025-03-05 21:17:18 -0300

    dakdskadjkakdw

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -54,9 +54,9 @@ const Cart = () => {
     const productDescriptions = cart.cartItems
       .map(
         (cartItems) =>
-          `${cartItems.quantity}${cartItems.item.title.slice(0, 10)} ${cartItems.size} ${cartItems.color}`
+          `${cartItems.quantity}${cartItems.item.title.slice(0, 10)}${cartItems.size}${cartItems.color}`
       )
-      .join("; ");
+      .join(";");
 
     router.push(
       `/pix?total=${totalWithDiscountRounded}&desc=${encodeURIComponent(
